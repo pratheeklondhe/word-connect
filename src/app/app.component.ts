f30b8ac2c02b5242bb36d499bfb0e85e59f7952f
pratheeklondhe <41989079+pratheeklondhe@users.noreply.github.com>
2023-03-17 11:50:39 +0530

    3 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { GameService } from 'src/services/game.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  name = 'Ionic 6.2 Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  puzzle: string[][] = [];
 
-  constructor() {}
+  constructor(private gameService: GameService) {}
 
-  ionViewDidEnter() {}
+  async ngOnInit() {
+    this.puzzle = await this.gameService.generatePuzzle();
+  }
+
+  // Add methods for handling user input, such as selecting letters and submitting words.
 }
